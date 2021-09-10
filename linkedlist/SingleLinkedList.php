26742669a1d5cd00af90f0f3046adf34d11bc4b1
v_kkxli <v_kkxli@tecent.com>
2021-09-10 17:38:21 +0800

    删除链表倒数第n个节点

--- linkedlist/SingleLinkedList.php
+++ linkedlist/SingleLinkedList.php
@@ -52,6 +52,44 @@ class SingleLinkedList
         return $newNode;
     }
     
+    //链表中环的检测
+    
+    //删除链表倒数第 n 个结点
+    public function removeNthFromEnd($n)
+    {
+        $fast = $this->head;
+        $slow = $this->head;
+        
+        while($n--)
+        {
+            $fast = $fast->next;
+        }
+        
+        while($fast->next)
+        {
+            $fast = $fast->next;
+            $slow = $slow->next;
+        }
+        
+        $slow->next = $slow->next->next;
+    }
+    
+    //链表的中间结点
+    public function getMidNode()
+    {
+        $slow = $this->head->next;
+        $fast = $this->head->next;
+        
+        while($fast && $fast->next)
+        {
+            $fast = $fast->next->next;
+            $slow = $slow->next;
+        }
+        
+        return $slow;
+    }
+    
+    //单链表反转
     public function reverse()
     {
         if ($this->getLength() <= 0) return false;
@@ -101,7 +139,10 @@ class SingleLinkedList
     //head->c->b->a->null 
     public function printData()
     {
-        if (null == $this->head->next) return false;
+        if (null == $this->head->next) {
+             echo 'NULL';
+             return false;
+        }
         
         $curNode = $this->head;
         
@@ -123,7 +164,7 @@ function isPalindrome(SingleLinkedList $list)
     
     $slow = $list->head->next;
     $fast = $list->head->next;
-    $pre = $remain = null;
+    $pre = null;
     
     while($fast != null && $fast->next != null){
         //快指针走2步
@@ -152,21 +193,32 @@ function isPalindrome(SingleLinkedList $list)
     return true;
 }
 
+//两个有序的链表合并
+
+
 $singleLinedList = new SingleLinkedList();
 $singleLinedList->insert('a');
 $singleLinedList->insert('b');
 $singleLinedList->insert('c');
 $singleLinedList->insert('d');
 $singleLinedList->insert('e');
+$singleLinedList->insert('f');
 $singleLinedList->printData();
+
+$singleLinedList->removeNthFromEnd(1);
+$singleLinedList->printData();
+
+/*
+var_dump($singleLinedList->getMidNode());
 
 //链表反转
 $singleLinedList->reverse();
 $singleLinedList->printData();
 
-//链表不分反转
+//链表部分反转
 $singleLinedList->reverseBetween(2, 3);
 $singleLinedList->printData();
 
 //回文串判断
 var_dump(isPalindrome($singleLinedList));
+*/
